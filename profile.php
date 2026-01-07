17832150fd4bdbedde6141d1328a29a6928efb2e
Faheer Hakim <r0820158@student.thomasmore.be>
2026-01-07 22:25:31 +0100

    feat(profile): add session check to redirect unauthenticated users to login

--- profile.php
+++ profile.php
@@ -1,4 +1,10 @@
-<!DOCTYPE html>
+<?php
+session_start();
+if(!isset($_SESSION['username'])) {
+     header("Location: login.php");
+    exit();
+}
+?><!DOCTYPE html>
 <html lang="en">
 <head>
     <title>User profile</title>
